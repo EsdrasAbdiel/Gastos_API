4480104d58e1d8493763af37d6285160f3332186
Esdras Diogo <Esdras.Diogo@fagrontech.com.br>
2026-02-10 19:13:20 -0300

    Adicionando Models/RegistroRequest

---
+++ Gastos API/Models/RegistroRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Gastos_API.Models
+{
+    public class RegistroRequest
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public int Sexo { get; set; }
+        public string? Descricao { get; set; }
+        public string Senha { get; set; }
+        public string ConfirmacaoSenha { get; set; }
+    }
+}
